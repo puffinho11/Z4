1274b0430a0605e4f259a8d9eee4ec340c047295
Luiz Otávio Puff <ens-luizpuff@ugv.edu.br>
2025-12-24 17:18:28 -0300

    24-12

--- futsal-score/backend/server.js
+++ futsal-score/backend/server.js
@@ -17,6 +17,8 @@ import timeRoutes from "./routes/timeRoutes.js"
 dotenv.config()
 
 const app = express()
+
+
 app.use(express.json())
 
 app.use(
@@ -39,11 +41,6 @@ const __dirname = path.dirname(__filename)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 app.use("/uploads/foto", express.static(path.join(__dirname, "uploads/foto")))
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Conectado ao MongoDB Atlas"))
-  .catch((err) => console.error("❌ Erro ao conectar ao MongoDB:", err))
-
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/registros", registroRoutes)
@@ -59,14 +56,14 @@ app.get("/", (req, res) => {
 app.get("/api/test", (req, res) => {
   res.json({
     status: "online",
-    message: "🚀 Backend Z4 está rodando corretamente!",
     mongo:
       mongoose.connection.readyState === 1
         ? "🟢 Conectado ao MongoDB"
         : "🔴 Desconectado do MongoDB",
     time: new Date().toLocaleString("pt-BR")
-  })
-})
+  });
+});
+
 
 const register = new client.Registry()
 client.collectDefaultMetrics({ register })
@@ -76,24 +73,21 @@ app.get("/metrics", async (req, res) => {
   res.end(await register.metrics())
 })
 
-const DEFAULT_PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 
-function startServer(port) {
-  const server = app.listen(port, () => {
-    console.log(`🚀 Servidor rodando na porta ${port}`)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Conectado ao MongoDB Atlas")
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando na porta ${PORT}`)
+    })
   })
-
-  server.on("error", (err) => {
-    if (err.code === "EADDRINUSE") {
-      console.warn(`⚠️ Porta ${port} em uso. Tentando porta ${port + 1}...`)
-      startServer(Number(port) + 1)
-    } else {
-      throw err
-    }
+  .catch((err) => {
+    console.error("❌ Erro ao conectar ao MongoDB:", err.message)
+    process.exit(1)
   })
-}
-
-startServer(DEFAULT_PORT)
 
 
 
